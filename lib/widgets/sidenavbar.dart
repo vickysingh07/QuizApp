55e4a5c47540fbefa0d9858c0c8330e5f514a594
Vicky Singh <vicky1910098@akgec.ac.in>
2022-01-28 02:10:17 +0530

    third commit

--- lib/widgets/sidenavbar.dart
+++ lib/widgets/sidenavbar.dart
@@ -1,76 +1,138 @@
+// ignore_for_file: await_only_futures, use_key_in_widget_constructors, invalid_required_positional_param, must_be_immutable, prefer_const_declarations, prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:kbc_app/services/auth.dart';
+import 'package:kbc_app/views/home.dart';
+import 'package:kbc_app/views/login.dart';
+import 'package:kbc_app/views/profile.dart';
 
 class SideNav extends StatelessWidget {
-  const SideNav({Key? key}) : super(key: key);
+  String name;
+  String money;
+  String rank;
+  String proUrl;
+  String level;
+  SideNav(@required this.name, @required this.money, @required this.rank,
+      @required this.proUrl, @required this.level);
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: Material(
-        color: const Color.fromRGBO(128, 0, 128, 0.85),
+        color: Color.fromRGBO(128, 0, 128, 1),
         child: ListView(
-          //padding: const EdgeInsets.symmetric(horizontal: 20),
+          // padding:  EdgeInsets.symmetric(horizontal: 20),
           children: [
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              child: Row(
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            Profile(name, rank, money, proUrl, level)));
+              },
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  // ignore: prefer_const_constructors
-                  CircleAvatar(
-                    radius: 30,
-                    backgroundColor: Colors.white,
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 30,
+                          backgroundImage: NetworkImage(proUrl),
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              name,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              "Rs. $money",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 15),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
                   ),
-                  const SizedBox(
-                    width: 20,
-                  ),
-                  Column(
-                    // ignore: prefer_const_literals_to_create_immutables
-                    children: [
-                      const Text(
-                        "Vicky Singh",
-                        style: TextStyle(
+                  Container(
+                      padding: EdgeInsets.only(left: 25),
+                      child: Text("Leaderboard - $rank th Rank",
+                          style: TextStyle(
                             color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      const Text("Rs. 5000",
-                          style: TextStyle(color: Colors.white, fontSize: 15))
-                    ],
-                  )
+                            fontSize: 19,
+                            fontWeight: FontWeight.bold,
+                          ))),
                 ],
               ),
             ),
-            Container(
-              padding: const EdgeInsets.only(left: 25),
-              child: const Text("Leaderboard - 230th rank",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold)),
+            SizedBox(
+              height: 24,
             ),
-            const SizedBox(
-              height: 48,
+            Divider(
+              thickness: 1,
+              indent: 10,
+              endIndent: 10,
             ),
-            listItem(label: "Daily Quiz", icon: Icons.quiz),
-            listItem(label: "Leaderboard", icon: Icons.leaderboard),
-            listItem(label: "How to use", icon: Icons.question_answer),
-            listItem(label: "About Us", icon: Icons.face_retouching_off_rounded)
+            SizedBox(
+              height: 24,
+            ),
+            listItem(
+                context: context,
+                path: MaterialPageRoute(
+                    builder: (BuildContext context) => Home()),
+                label: "DAILY QUIZ",
+                icon: Icons.quiz),
+            listItem(
+                context: context,
+                path: MaterialPageRoute(
+                    builder: (BuildContext context) => Home()),
+                label: "Leaderboard",
+                icon: Icons.leaderboard),
+            listItem(
+                context: context,
+                path: MaterialPageRoute(
+                    builder: (BuildContext context) => Home()),
+                label: "How To Use",
+                icon: Icons.question_answer),
+            listItem(
+                context: context,
+                path: MaterialPageRoute(
+                    builder: (BuildContext context) => Home()),
+                label: "About Us",
+                icon: Icons.face),
+            listItem(
+                context: context,
+                path: MaterialPageRoute(
+                    builder: (BuildContext context) => Login()),
+                label: "Logout",
+                icon: Icons.logout)
           ],
         ),
       ),
     );
   }
 
-  Widget listItem({
-    required String label,
-    required IconData icon,
-  }) {
-    // ignore: prefer_const_declarations
+  Widget listItem(
+      {required String label,
+      required IconData icon,
+      required BuildContext context,
+      required MaterialPageRoute path}) {
     final color = Colors.white;
-    const hovercolor = Colors.white60;
+    final hovercolor = Colors.white60;
 
     return ListTile(
       leading: Icon(
@@ -79,6 +141,10 @@ class SideNav extends StatelessWidget {
       ),
       hoverColor: hovercolor,
       title: Text(label, style: TextStyle(color: color)),
+      onTap: () async {
+        await signOut();
+        Navigator.pushReplacement(context, path);
+      },
     );
   }
 }
